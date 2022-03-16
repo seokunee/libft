06e65a98d8c893a9beb88b55593364ca5e2c16d7
seokunee <xocoolree@github.com>
2022-03-17 02:15:34 +0900

    3/16

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,36 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seokhun <seokhun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/16 22:51:07 by seokhun           #+#    #+#             */
-/*   Updated: 2022/03/17 00:55:23 by seokhun          ###   ########.fr       */
+/*   Created: 2022/03/16 23:19:05 by seokhun           #+#    #+#             */
+/*   Updated: 2022/03/17 00:59:12 by seokhun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t len)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
+	size_t	j;
 
 	i = 0;
-	if (len == 0)
-		return (0);
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < len - 1)
+	j = 0;
+	if (little[0] == '\0')
+		return (big);
+	while (big[i])
+	{
+		if (big[i] == little[j])
+		{
+			while (big[i + j] == little[j] && big[i + j] && little[j] && j < len)
+				j++;
+			if (j == len)
+				return (big + i);
+			j = 0;
+		}
 		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
-
-#include<stdio.h>
-#include<string.h>
-int	main(void)
-{
-	char s1[] = "hello world";
-	char s2[] = "hello holla";
-	printf("%d\n", strncmp(s1, s2, 7));
-	// printf("%d\n", ft_strncmp(s1, s2, 7));
+	}
 	return (0);
 }
